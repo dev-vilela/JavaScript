6e0d45f799b791ece7e2508ba61b7dc6ab09ed15
Paulovilela <paulov4@hotmail.com>
2022-05-13 19:21:25 -0300

    dsf

---
+++ aula 15/script.js
@@ -0,0 +1,31 @@
+let num =  document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+       
+
+    }else{
+        window.alert('Valor inválido ou já enontrado na lista.')
+    }
+
+}
