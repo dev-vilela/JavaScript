633416494d205840c1f96be728e7ad1d720d2146
Paulo Vilela <95303442+dev-vilela@users.noreply.github.com>
2023-10-06 19:35:51 -0300

    att

---
+++ Curso de JavaScript web/Aula 03/index.js
@@ -0,0 +1,10 @@
+/*---------------------DECLARAÇÕES-SINTAXE------------------------------*/
+
+var vel = 50;
+console.log(`A velocidade do veiculo é ${vel} Km/h.`)
+
+if(vel > 60){
+    console.log("Veiculo ultrapassou o limite de velocidade. MULTADO");
+}else{
+    console.log("Veiculo no limite da via.")
+}
