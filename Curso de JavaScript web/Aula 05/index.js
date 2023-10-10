6e1122261ddd09e63a4c76543c68921109044f77
Paulo Vilela <95303442+dev-vilela@users.noreply.github.com>
2023-10-10 19:52:58 -0300

    a

---
+++ Curso de JavaScript web/Aula 05/index.js
@@ -0,0 +1,9 @@
+/*--------------------------Variáveis VAR, LET, CONST ----------------------------------*/
+
+var nome, sobrenome, nomeCompleto;
+
+nome = "Paulo";
+sobrenome = "Vilela";
+nomeCompleto = (nome + " " + sobrenome);
+
+document.getElementById("texto").innerHTML = nomeCompleto
